91d57ed605a5ca5a3ba2f73733ee48af2eabd365
Kevin Manca <72861758+kevinm6@users.noreply.github.com>
2025-03-08 23:28:21 +0100

    feat: add folds queries for tree-sitter

---
+++ queries/folds.scm
@@ -0,0 +1,16 @@
+;; Foldable block directives
+(if) @fold
+(elseif) @fold
+(else) @fold
+(switch) @fold
+(case) @fold
+(default) @fold
+(list) @fold
+(macro) @fold
+(function) @fold
+(attempt) @fold
+(recover) @fold
+(user_defined) @fold
+(assign) @fold
+(global) @fold
+(local) @fold
